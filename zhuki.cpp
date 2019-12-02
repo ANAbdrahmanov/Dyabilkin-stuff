3fe76271e9942d41df34693b991cad424f9e2cf2
ANAbdrahmanov <big.daddy5674@gmail.com>
2019-12-02 13:55:22 +0300

    Add files via upload

---
+++ zhuki.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <math.h>
+
+int& minRef(int& a, int& b) {
+	return a < b ? a : b;
+}
+
+
+int main() {
+	int x, y;
+	std::cin >> x >> y;									//не уверен в каком виде ввод, сделал просто последовательные 2 числа себе для удобства
+	
+	int center = x / 2 - 1 + (x & 1);					//вместо проверки на четность с условием в вычислении
+	
+	int currentDepth = 0;								//глубина шага
+	int exponent = 0;									//глубина "дерева"
+	for (int i = 0; i < y; i++) {
+		if (currentDepth < pow(2, exponent)) {
+			currentDepth++;
+		}
+		else {
+			currentDepth = 1;							//сразу закидываем
+			exponent++;
+			x = center;
+			center = x / 2 - 1 + (x & 1);
+		}
+	}
+	int firstOutput = center;
+	int secondOutput = x - center - 1;
+	if (x > 0 && currentDepth == 1 && exponent > 0) {
+		minRef(firstOutput, secondOutput)++;
+	}
+	std::cout << firstOutput << " " << secondOutput << "\n";	//порядок не имеет значения
+	system("pause");
+	return 0;
+}
