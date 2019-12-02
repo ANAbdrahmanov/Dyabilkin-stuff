06e5fcb5298fd295f3ca7bb17cecbbd65c47588a
ANAbdrahmanov <big.daddy5674@gmail.com>
2019-12-02 14:24:47 +0300

    update

--- zhuki.cpp
+++ zhuki.cpp
@@ -10,8 +10,8 @@ int main() {
 	int x, y;
 	std::cin >> x >> y;									//не уверен в каком виде ввод, сделал просто последовательные 2 числа себе для удобства
 	
-	int center = x / 2 - 1 + (x & 1);					//вместо проверки на четность с условием в вычислении
-	
+	int center = x / 2 - 1 + (x & 1);					//вместо проверки на четность с условием, все в вычислении
+	bool even = x % 2 == 0;								//это только для финальной проверки
 	int currentDepth = 0;								//глубина шага
 	int exponent = 0;									//глубина "дерева"
 	for (int i = 0; i < y; i++) {
@@ -27,9 +27,10 @@ int main() {
 	}
 	int firstOutput = center;
 	int secondOutput = x - center - 1;
-	if (x > 0 && currentDepth == 1 && exponent > 0) {
+	if (x > 0 && currentDepth == 1 && exponent > 0 && even) {
 		minRef(firstOutput, secondOutput)++;
 	}
+
 	std::cout << firstOutput << " " << secondOutput << "\n";	//порядок не имеет значения
 	system("pause");
 	return 0;
